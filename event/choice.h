190d2899864bee565dbb3c634c202423d27fb3ea
Musickenoo <95070991+Musickenoo@users.noreply.github.com>
2024-03-01 01:44:31 +0700

    Update choice.h

--- event/choice.h
+++ event/choice.h
@@ -37,21 +37,21 @@ int Relation() {
     // ดึงค่าข้อมูลตัวละคร
     Venti Venti;
 
-    switch(Charactor){
-        case 'Venti' :  charactorTalkMessege     = Venti.ventiTalk;
-                        userTalkMessege          = Venti.userTalk; 
-                        charactorQuestionMessage = Venti.ventiQuestions;
-                        userAnswerMessage        = Venti.userAnswers;
-                        charactorActionMessage   = Venti.ventiAction;
-                        actionRelation           = Venti.actionRelation;
-                        waifuface1Texture        = Venti.waifuface1Texture; 
-                        waifuface2Texture        = Venti.waifuface2Texture;
-                        waifuface3Texture        = Venti.waifuface3Texture;
-                        waifuface4Texture        = Venti.waifuface4Texture;
-                        waifuface5Texture        = Venti.waifuface5Texture;
-                        waifuface6Texture        = Venti.waifuface6Texture;
-                        break;
-        case 'e'     :  break;
+    switch (Charactor) {
+    case 'Venti':  charactorTalkMessege = Venti.ventiTalk;
+        userTalkMessege = Venti.userTalk;
+        charactorQuestionMessage = Venti.ventiQuestions;
+        userAnswerMessage = Venti.userAnswers;
+        charactorActionMessage = Venti.ventiAction;
+        actionRelation = Venti.actionRelation;
+        waifuface1Texture = Venti.waifuface1Texture;
+        waifuface2Texture = Venti.waifuface2Texture;
+        waifuface3Texture = Venti.waifuface3Texture;
+        waifuface4Texture = Venti.waifuface4Texture;
+        waifuface5Texture = Venti.waifuface5Texture;
+        waifuface6Texture = Venti.waifuface6Texture;
+        break;
+    case 'e':  break;
     }
 
     waifu2.setTexture(waifuface1Texture);
@@ -130,22 +130,11 @@ int Relation() {
             }
         }
     }
-    window.draw(charactorTalk);
+  
     for (int Day = 0; Day < finalDay; Day++) {
-        for (int basicTalk = 0; basicTalk < endBasicTalk; basicTalk++) 
+        for (int basicTalk = 0; basicTalk < endBasicTalk; basicTalk++){ 
             while (true) {
-                if (rectangle.getGlobalBounds().contains(mousePos)) {
-                    charactorTalk[Day][basicTalk].setPosition(600, 750);
-                    window.draw(charactorTalk[Day][basicTalk]);
-                    while (true) {
-                        if (rectangle.getGlobalBounds().contains(mousePos)) {
-                            userTalk[Day][basicTalk].setPosition(600, 750);
-                            window.draw(userTalk[Day][basicTalk]);
-                            break;
-                        }             
-                    }
-                    break;
-                }
+                
             }
         }
         for (int Question = 0; Question < charactorQuestionMessage[Day].size(); Question++) {
@@ -161,16 +150,16 @@ int Relation() {
                     //if(เลือกคำตอบที่ 2) action = 1;
                     //if(เลือกคำตอบที่ 3) action = 2;
                     // put text ขึ้นจอตรงนี้
-                    cout << charactorActionMessage[Day][Question][action]; // เทส
-                    relation += actionRelation[Day][Question][action];
-                    break; 
-                }
-                break; // เทส
+                //cout << charactorActionMessage[Day][Question][action]; // เทส
+                //relation += actionRelation[Day][Question][action];
+                break;
             }
-            cin.get(); // ตัวเทส
+            break; // เทส
         }
-        cout << endl << charactorTalkMessege[Day][endBasicTalk] << endl; // เทส
-        cout << userTalkMessege[Day][endBasicTalk] << endl << endl; // เทส  
+        cin.get(); // ตัวเทส
+    cout << endl << charactorTalkMessege[Day][endBasicTalk] << endl; // เทส
+    cout << userTalkMessege[Day][endBasicTalk] << endl << endl; // เทส  
     }
-    return relation;
+
+return relation;
 }
