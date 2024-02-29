d4e7b033a617a93caf3adf1036740f29c5c5de3c
mannaja <mannaja>
2024-03-01 02:03:16 +0700

    no erorr

--- event/choice.h
+++ event/choice.h
@@ -44,12 +44,12 @@ int Relation() {
                         userAnswerMessage        = Venti.userAnswers;
                         charactorActionMessage   = Venti.ventiAction;
                         actionRelation           = Venti.actionRelation;
-                        waifuface1Texture        = Venti.waifuface1Texture; 
-                        waifuface2Texture        = Venti.waifuface2Texture;
-                        waifuface3Texture        = Venti.waifuface3Texture;
-                        waifuface4Texture        = Venti.waifuface4Texture;
-                        waifuface5Texture        = Venti.waifuface5Texture;
-                        waifuface6Texture        = Venti.waifuface6Texture;
+                        waifuface1Texture        = Venti.V1;
+                        waifuface2Texture        = Venti.V2;
+                        waifuface3Texture        = Venti.V3;
+                        waifuface4Texture        = Venti.V4;
+                        waifuface5Texture        = Venti.V5;
+                        waifuface6Texture        = Venti.V6;
                         break;
         case 'e'     :  break;
     }
@@ -91,6 +91,13 @@ int Relation() {
         for (int Question = 0; Question < charactorQuestionMessage[Day].size(); Question++) {
             s = charactorQuestionMessage[Day][Question];
             text.setFont(init.Thai);
+            text.setString(s);
+            text.setCharacterSize(60);
+            text.setPosition(1600,900);
+            text.setFillColor(Color::Black);
+            charactorQuestions[Day].push_back(text);
+        }
+    }
     vector<vector<vector<Text>>> userAnswers(userAnswerMessage.size());  // Resize the vector
     for (int Day = 0; Day < userAnswerMessage.size(); Day++) {
         userAnswers[Day].resize(userAnswerMessage[Day].size());  // Resize the inner vector
@@ -144,16 +151,16 @@ int Relation() {
                     //if(เลือกคำตอบที่ 2) action = 1;
                     //if(เลือกคำตอบที่ 3) action = 2;
                     // put text ขึ้นจอตรงนี้
-                    cout << charactorActionMessage[Day][Question][action]; // เทส
-                    relation += actionRelation[Day][Question][action];
+                    //cout << charactorActionMessage[Day][Question][action]; // เทส
+                    //relation += actionRelation[Day][Question][action];
                     break; 
-                }
-                break; // เทส
             }
-            cin.get(); // ตัวเทส
+            break; // เทส
         }
+        cin.get(); // ตัวเทส
         cout << endl << charactorTalkMessege[Day][endBasicTalk] << endl; // เทส
         cout << userTalkMessege[Day][endBasicTalk] << endl << endl; // เทส  
     }
+
     return relation;
 }
